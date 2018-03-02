065df087fcb429f0a90438cb760f5563aaa70bab
PaolaBravo01 <36998217+PaolaBravo01@users.noreply.github.com>
2018-03-02 13:01:22 -0500

    Crear metodo imprimir

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,13 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("TENGO MUCHA HAMBREEEE");
+		
+		
+	}
+
+	public static void imprimir(String[] str)
+	{
+		System.out.println(str);
 	}
 
 }
